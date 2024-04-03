32c420263cd922f9eb29966abad412e84a4210ea
hatamoni <hatim.ibrahim@yahoo.de>
2024-04-03 17:30:47 +0200

    chnges due to aws variables and entities

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -27,9 +27,9 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "gitopsterrastate"
+    bucket = "vprofileactionshixdev"
     key    = "terraform.tfstate"
-    region = "us-east-2"
+    region = var.region
   }
 
   required_version = "~> 1.6.3"
